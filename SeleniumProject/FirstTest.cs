a0027336c9a13176365d1e6852ddbcbea9a066d9
Joshua Kemp <joshua.kemp@avanade.com>
2018-01-18 11:26:52 -0500

    First commit, add gitignore file

---
+++ SeleniumProject/FirstTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject
+{
+    public class FirstTest
+    {
+        IWebDriver driver = new FirefoxDriver();
+
+        [Test]
+        public void myFirstTest()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Assert.AreEqual("Google", driver.Title);
+
+            driver.Close();
+
+            driver.Quit();
+
+        }
+    }
+}
